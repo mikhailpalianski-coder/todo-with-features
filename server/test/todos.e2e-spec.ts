73a5ea355fa1317f1937c3585c6a604c7d5cd03c
Mike <mike@example.com>
2025-11-13 00:43:33 +0300

    fix issues in back end side to ci

--- server/test/todos.e2e-spec.ts
+++ server/test/todos.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '../src/app.module';
 
 describe('TodosController (e2e)', () => {
@@ -29,19 +29,25 @@ describe('TodosController (e2e)', () => {
 
   describe('/todos (POST)', () => {
     it('should create a todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .post('/todos')
         .send({ content: 'E2E Test Todo' })
         .expect(201)
         .expect((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body).toHaveProperty('_id');
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body.content).toBe('E2E Test Todo');
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body.completed).toBe(false);
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
           createdTodoId = res.body._id;
         });
     });
 
     it('should fail to create todo without content', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .post('/todos')
         .send({})
@@ -51,10 +57,12 @@ describe('TodosController (e2e)', () => {
 
   describe('/todos (GET)', () => {
     it('should return an array of todos', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .get('/todos')
         .expect(200)
         .expect((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(Array.isArray(res.body)).toBe(true);
         });
     });
@@ -62,16 +70,20 @@ describe('TodosController (e2e)', () => {
 
   describe('/todos/:id (GET)', () => {
     it('should return a todo by id', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .get(`/todos/${createdTodoId}`)
         .expect(200)
         .expect((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body._id).toBe(createdTodoId);
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body.content).toBe('E2E Test Todo');
         });
     });
 
     it('should return 404 for non-existent todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .get('/todos/507f1f77bcf86cd799439011')
         .expect(404);
@@ -80,16 +92,19 @@ describe('TodosController (e2e)', () => {
 
   describe('/todos/:id (PATCH)', () => {
     it('should update a todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .patch(`/todos/${createdTodoId}`)
         .send({ completed: true })
         .expect(200)
         .expect((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body.completed).toBe(true);
         });
     });
 
     it('should return 404 for non-existent todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .patch('/todos/507f1f77bcf86cd799439011')
         .send({ completed: true })
@@ -99,15 +114,18 @@ describe('TodosController (e2e)', () => {
 
   describe('/todos/:id (DELETE)', () => {
     it('should delete a todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .delete(`/todos/${createdTodoId}`)
         .expect(200)
         .expect((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           expect(res.body._id).toBe(createdTodoId);
         });
     });
 
     it('should return 404 for non-existent todo', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       return request(app.getHttpServer())
         .delete('/todos/507f1f77bcf86cd799439011')
         .expect(404);
